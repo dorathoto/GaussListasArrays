8a9352d1431b6f7bfff813725e4c010c63ff0c41
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2023-04-27 14:17:21 -0300

    dicas1

--- ListasArrays/Program.cs
+++ ListasArrays/Program.cs
@@ -1,57 +1,18 @@
-﻿var nomes = new List<string>();
-nomes.Add("Barbara");
-nomes.Add("Maria");
-nomes.Add("Ana");
+﻿//exemplo de var
+
+var valor1 = 10;
+var valor2 = 20m;
+var valor3 = 30.0;
+var valor4 = 40.0f;
+
+//variaves menos comuns
+var valor5 = 50L;
+var valor6 = 60u;
+var valor7 = 70ul;
+var valor8 = 80uL;
 
 
-var alunasList = new List<(string nome, int idade)>();
-alunasList.Add(("Barbara", 16));
-alunasList.Add(("Maria", 20));
-alunasList.Add(("Ana", 19));
 
-
-var alunoList2 = new List<(string nome, int idade)>
-{
-    ("Barbara", 16),
-    ("Maria", 20),
-    ("Ana", 19)
-};
-
-//LINQ
-//Orderby, OrderbyDesc, Find, lambda
-
-var alunoNull = alunasList.Find(x => x.nome == "Leonardo");
-
-var alunoAchou = alunasList.Find(x => x.nome == "João");
-var idadeAlunoAchou = alunoAchou.idade;
-
-var existe = alunasList.Any(w => w.idade == 18);
-
-
-//.WHERE ToList(), First()
-var t = alunasList.Where(w => w.idade != 20 && w.nome.Contains("a")).ToList();
-
-//SUM - somar
-var totalIdade = alunasList.Sum(w => w.idade);
-
-//WHERE + SUM
-var totalIdade2 = alunasList.Where(w => w.idade > 18).Sum(w => w.idade);
-
-//Somente um campo
-var listString = alunasList.Select(p => p.nome).ToList();
-
-foreach (var aluno in alunasList.OrderBy(o => o.nome))
-{
-    Console.WriteLine(aluno.nome);
-
-}
-
-//escopo de variavel aluno já existe mas pertence somente ao foreach
-foreach (var aluno in alunasList.OrderByDescending(o => o.idade))
-{
-    Console.WriteLine($"Idade {aluno.idade}");
-
-}
 
 
 
