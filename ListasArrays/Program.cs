b709cd13c2b77df83a4488411e94f0048be27c74
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2023-04-27 11:25:45 -0300

    operadores

--- ListasArrays/Program.cs
+++ ListasArrays/Program.cs
@@ -1,83 +1,86 @@
-﻿namespace ListasArrays
-{
-    internal class Program
-    {
-        public class Livro
-        {
-            //construtor da classe já recebendo 2 parametros
-            public Livro(string Receita, string Categoria)
-            {
-                this.Receita = Receita;
-                this.Categoria = Categoria;
-            }
+﻿using System.ComponentModel.DataAnnotations;
 
-            //aqui são propriedades da classe
+int valor1 = 10;
+int valor2 = 3;
 
-            public string Receita { get; set; }
-            public string Categoria { get; set; }
-        }
+//Operadores aritméticos
+int atribuicao = valor1 + valor2;
+int subtracao = valor1 - valor2;
+int multiplicacao = valor1 * valor2;
+int divisao = valor1 / valor2;
+int resto = valor1 % valor2;
 
+Console.WriteLine($"Atribuição de valor1 + valor2 =  {atribuicao}");
+Console.WriteLine($"Subtração de valor1 - valor2 =  {subtracao}");
+Console.WriteLine($"Multiplicação de valor1 * valor2 =  {multiplicacao}");
+Console.WriteLine($"Divisão de valor1 / valor2 =  {divisao}");
+Console.WriteLine($"Resto de valor1 % valor2 =  {resto}");
 
 
+Console.WriteLine("==========================================");
+///Operadores de Atribuição reduzida +=, -=, *=, /=, %=
+int valor3 = valor1;
+int valor4 = valor2;
 
-        static void Main(string[] args)
-        {
-            //não tinha pq ser uma variavel fora da main e static, trouxe pra cá.
-            var receitasBolo = new List<Livro>();
-
-
-            Console.WriteLine("Receita de bolo? [para sim digite 1]");
-            int digite = Int32.Parse(Console.ReadLine());
-            if (digite == 1)
-            {
-                receitasBolo.Add(new Livro("brigadeiro", "bolo"));
-                receitasBolo.Add(new Livro("morango", "bolo"));
-                receitasBolo.Add(new Livro("Laranja", "bolo"));
-                receitasBolo.Add(new Livro("Cenoura", "bolo"));
-
-                //coloquei as chaves
-                foreach (Livro l in receitasBolo)
-                {
-                    Console.WriteLine(l.Categoria + " de " + l.Receita);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Vamos para a próxima categoria!");
-            }
-            //removi umas chaves perdidas
+int atribuicao2 = valor3 += valor4;
+int subtracao2 = valor3 -= valor4;
+int multiplicacao2 = valor3 *= valor4;
+int divisao2 = valor3 /= valor4;
+int resto2 = valor3 %= valor4;
 
 
-            Console.WriteLine("\n");
+Console.WriteLine($"Atribuição de valor3 += valor4 =  {atribuicao2}");
+Console.WriteLine($"Subtração de valor3 -= valor4 =  {subtracao2}");
+Console.WriteLine($"Multiplicação de valor3 *= valor4 =  {multiplicacao2}");
+Console.WriteLine($"Divisão de valor3 /= valor4 =  {divisao2}");
+Console.WriteLine($"Resto de valor3 %= valor4 =  {resto2}");
+
+Console.WriteLine("==========================================");
+///Operadores de Incremento e Decremento e pré-incremento, pré-decremento
+int valor5 = valor1++;
+int valor6 = valor1--;
+int valor7 = ++valor1;
+int valor8 = --valor1;
+Console.WriteLine($" valor1++ =  {valor5}");
+Console.WriteLine($" valor1-- =  {valor6}");
+Console.WriteLine($" ++valor1 =  {valor7}");
+Console.WriteLine($" --valor1 =  {valor8}");
+
+Console.WriteLine("==========================================");
+///Operadores de Comparação ==, !=, >, <, >=, <=
+bool comparacao1 = valor1 == valor2;
+bool comparacao2 = valor1 != valor2;
+bool comparacao3 = valor1 > valor2;
+bool comparacao4 = valor1 < valor2;
+bool comparacao5 = valor1 >= valor2;
+bool comparacao6 = valor1 <= valor2;
+Console.WriteLine($"valor1 == valor2 =  {comparacao1}");
+Console.WriteLine($"valor1 != valor2 =  {comparacao2}");
+Console.WriteLine($"valor1 > valor2 =  {comparacao3}");
+Console.WriteLine($"valor1 < valor2 =  {comparacao4}");
+Console.WriteLine($"valor1 >= valor2 =  {comparacao5}");
+Console.WriteLine($"valor1 <= valor2 =  {comparacao6}");
 
 
-            List<Livro> receitasTorta = new List<Livro>();
-            Console.WriteLine("Receita de Torta? [para sim digite 1]");
-            int escreva = Int32.Parse(Console.ReadLine());
-            if (escreva == 1)
-            {
-                receitasTorta.Add(new Livro("frango", "Torta"));
-                receitasTorta.Add(new Livro("carne", "Torta"));
-                receitasTorta.Add(new Livro("morango", "Torta"));
-                receitasTorta.Add(new Livro("banana", "Torta"));
-                receitasTorta.Add(new Livro("batata", "Torta"));
-                receitasTorta.Add(new Livro("doce", "Torta"));
+Console.WriteLine("==========================================");
+//Operadores Lógicos &&, ||, !
 
-                foreach (Livro t in receitasTorta)
-                {
-                    Console.WriteLine(t.Categoria + " de " + t.Receita);
-                }
-            }
-            else
-            {
-                Console.WriteLine("sem mais receitas");
-            }
+bool logico1 = valor1 == valor2 && valor1 > valor2;
+bool logico2 = valor1 == valor2 || valor1 > valor2;
+bool logico3 = !(valor1 == valor2);
+Console.WriteLine($"valor1 == valor2 && valor1 > valor2 =  {logico1}");
+Console.WriteLine($"valor1 == valor2 || valor1 > valor2 =  {logico2}");
+Console.WriteLine($"!(valor1 == valor2) =  {logico3}");
 
 
-        }
-    }
-}
+Console.WriteLine("==========================================");
+
+
+//Operador ternário
+
+var ternario = true;
+var logico4 = ternario ? valor1 : valor2;
 
 
 
-
+Console.ReadKey();
